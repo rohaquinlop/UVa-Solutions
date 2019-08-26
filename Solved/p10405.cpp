1db39e20bb97e6326b1d51a96525d9df4523adc4
Fockdislaif <rohaquinlop301@gmail.com>
2019-08-26 13:39:39 -0500

    10405 - Longest Common Subsequence

---
+++ Solved/p10405.cpp
@@ -0,0 +1,42 @@
+/*
+*
+* Problema UVA 10405 - Longest Common Subsequence
+*
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int max(int a, int b){return (a>b)? a : b;}
+
+int LCIS(string X, int m, string Y, int n){
+	int L[m+1][n+1];
+
+	for(int i = 0; i <= m; i++){
+		for(int j = 0; j <= n; j++){
+			if (i == 0 || j == 0) 
+				L[i][j] = 0; 
+
+			else if (X[i-1] == Y[j-1]) 
+				L[i][j] = L[i-1][j-1] + 1; 
+
+			else
+				L[i][j] = max(L[i-1][j], L[i][j-1]);
+		}
+	}
+
+	return L[m][n];
+}
+
+int main(){
+	string line1, line2;
+
+	while( getline(cin, line1) ){
+		getline(cin, line2);
+
+		printf("%d\n", LCIS(line1, line1.size(), line2, line2.size()));
+	}
+
+	return 0;
+}
