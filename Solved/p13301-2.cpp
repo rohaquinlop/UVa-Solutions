1cce36782e596969c4d51e1bf85f1bb01085bab9
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2019-12-07 09:16:45 -0500

    13301 - Impossible Communication

--- Solved/p13301-2.cpp
+++ Solved/p13301-2.cpp
@@ -12,7 +12,55 @@
 using namespace std; 
 
 int countSCCs;
-  
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+
+typedef vector < pii > vpii;
+typedef vector < pll > vpll;
+
+typedef vector <string> vs;
+typedef vector < vi > vvi;
+typedef vector < vll > vvll;
+
+#define fl(i, a, b)	for (int i(a); i < (b); ++i)
+
+#define all(v) 	(v).begin(), (v).end()
+#define srt(v)	sort (all (v))
+
+#define pb push_back
+#define mp make_pair
+
+#define dig(i)	(s[i] - '0')
+#define slen(s)	s.length()
+
+#define fr first
+#define sc second
+
+#define sz(x) ((int) (x).size())
+#define fill (x, y)	memset(x, y, sizeof(x))
+#define clr(a)	fill(a, 0)
+#define endl '\n'
+
+#define PI 3.14159265358979323
+
+#define trace1(x1)					cerr << #x1 << ": " << x1 << endl;
+#define trace2(x1, x2)				cerr << #x1 << ": " << x1 << " | " << #x2 << ": " << x2 << endl;
+#define trace3(x1, x2, x3)			cerr << #x1 << ": " << x1 << " | " << #x2 << ": " << x2 << " | " << #x3 << ": " << x3 << endl;
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+const ll MOD = 1000000007LL;
+const ll MAX = 100010LL;
+
+template <typename T> T gcd(T a, T b) { if (b == 0) return a; return gcd(b, a % b);}
+template <typename T> T power(T x, T y, ll m = MOD) { T ans = 1; x %= m; while (y > 0) { if (y & 1ll) ans = (ans * x) % m; y >>= 1ll; x = (x * x)%m; } return ans%m;}
+
 // A class that represents an directed graph 
 class Graph{
     int V;    // No. of vertices 
@@ -97,17 +145,18 @@ void Graph::SCC(){
         }
 }
 
-int main(){
-	int n, m, k, i, j, l, aux, nInicio, nFin;
+int main() {
+	FAST_IO;
+    int n, m, k, i, j, l, aux, nInicio, nFin;
 
-	while( scanf("%d %d", &n, &m) == 2 ){
+	while( cin >> n >> m ){
 		countSCCs = 0;
 		Graph g(n);
-		for(i = 0; i < m; i++){
-			scanf("%d", &k);
+		fl(i, 0, m){
+			cin >> k;
 			if( k > 1 ){
-				for(j = 0; j < k; j++){
-					scanf("%d", &aux);
+				fl(j, 0, k){
+                    cin >> aux;
                     if( j == 0 ){
                         nInicio = aux;
                     }
@@ -119,13 +168,13 @@ int main(){
                 g.addEdge(nFin-1, nInicio-1);
 			}
 			else{
-				scanf("%d %d", &j, &l);
+				cin >> j >> l;
 				g.addEdge(j-1, l-1);
 			}
 		}
 		g.SCC();
-		if( countSCCs == 1 ) printf("YES\n");
-		else printf("NO\n");
+		if( countSCCs == 1 ) cout << "YES" << endl;
+		else cout << "NO" << endl;
 	}
 
 	return 0;
