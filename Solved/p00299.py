5d2c2d510e956e1995197103a0cc0c88908e14c9
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-02-23 16:02:12 -0500

    299 - Train Swapping

--- Solved/p00299.py
+++ Solved/p00299.py
@@ -1,39 +1,69 @@
-'''
+"""
 *
 * Problema UVa 299 - Train Swapping
 *
-'''
-
+"""
 from sys import *
-##import time
+count = 0
 
-def sort(lst):
-	count = 0
+def concatenate(a, b):
+  global count
+  res = []
+  i = 0
+  j = 0
+  while (i < len(a)) or (j < len(b)):
+    if i == len(a):
+      res.append(b[j])
+      j += 1
+    elif j == len(b):
+      res.append(a[i])
+      i += 1
+    else:
+      if a[i] <= b[j]:
+        res.append(a[i])
+        i += 1
+      else:
+        res.append(b[j])
+        count += len(a)-i
+        j += 1
+  return res
 
-	i = 0
-	while i < len(lst)-1:
-		a = lst[i]
-		b = lst[i+1]
-		if a > b:
-			lst[i], lst[i+1] = lst[i+1], lst[i]
-			i = 0
-			count += 1
-		else:
-			i += 1
-	return count
+def divideLst(lst):
+  res = []
+  mid = len(lst)//2
+  i = 0
+  while i < mid:
+    res.append(lst[i])
+    i += 1
+  return res, lst[i::]
+
+
+def mergeSort(lst):
+  if len(lst) == 1:
+    return lst
+  else:
+    a, b = divideLst(lst)
+    return concatenate(mergeSort(a), mergeSort(b))
 
 def main():
-	lines = stdin.readlines()
-	lines.pop(0)
-	out = ""
-	for i, line in enumerate(lines):
-		if (i+1)%2 == 0: 
-			count = 0
-			line = line.strip().split()
-			line = list(map(int, line))
-			out += "Optimal train swapping takes {} swaps.\n".format(sort(line))
-	print(out.strip())
+  global count
+  lines = stdin.readlines()
+  lines = list(map(int, (" ".join(map(lambda x: x.strip(), lines))).split()))
+  i = 1
+  out = ""
+  while i < len(lines):
+    count = 0
+    elem = lines[i]
+    nums = []
+    i += 1
+    j = 0
+    while j < elem:
+      nums.append(lines[i])
+      i += 1
+      j += 1
+    if elem > 0:
+      nums = mergeSort(nums)
+    out += "Optimal train swapping takes {0} swaps.\n".format(count)
+  print(out.strip())
 
-##startTime = time.time()
 main()
-##print("--- %s seconds ---" % (time.time() - startTime))
