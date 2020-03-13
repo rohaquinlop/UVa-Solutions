1d8f7fb19b4028ca7853a9411a68928bc085913f
Fockdislaif <rohaquinlop301@gmail.com>
2020-03-13 15:31:41 -0500

    12160 - Unlock the Lock

--- Solved/p12160.py
+++ Solved/p12160.py
@@ -6,29 +6,6 @@
 from collections import deque
 from sys import stdin
 
-def addStrings(s,t):
-  res = str(int(s)+int(t))
-  if len(res) >= 4:
-    v = res[::-1]
-    v = v[0:4]
-    v = v[::-1]
-    return v
-  else:
-    return res
-
-# def addStrings(s, t):
-#   res = ""
-#   i = 3
-#   carry = 0
-#   while i >= 0:
-#     v = int(s[i])+int(t[i])+carry
-#     res += str(v%10)
-#     carry = v//10
-#     i -= 1
-#   res = res[::-1]
-#   return res
-
-
 def main():
   lines = stdin.readlines()
   i = 0
@@ -39,23 +16,25 @@ def main():
     l = lines[i].strip().split()
     if l[0] == '0' and l[1] == '0' and l[2] == '0':
       break
-    L, U = l[0], l[1]
+    L, U = int(l[0]), int(l[1])
     i += 1
-    rV = lines[i].strip().split()
+    rV = list(map(int, lines[i].strip().split()))
     i += 1
 
     ## Do BFS since startNode to endNode taking endNode
     startNode, endNode = L, U
-    q = deque()
+    q = []
     q.append([startNode, 0])
     found = False
     steps = 0
     memo = dict()
     memo[startNode] = 1
+    j = 0
 
-    while len(q) > 0:
+    while j < len(q):
       ## End and stop
-      l = q.popleft()
+      l = q[j]
+      j += 1
       node = l[0]
       if node == endNode:
         steps = l[1]
@@ -63,7 +42,7 @@ def main():
         break
       
       for r in rV:
-        t = addStrings(node, r)
+        t = (node+r)%10000
         #q.append([t, l[1]+1])
         v = memo.get(t)
         if v == None:
