e199f463010057413b8c82bb55992e22cc4b7cba
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2020-08-16 20:06:10 -0500

    860 - Entropy Text Analyzer

---
+++ Solved/p00860 - Entropy Text Analyzer.cpp
@@ -0,0 +1,115 @@
+/*
+*
+* Problema UVa 860 - Entropy Text Analyzer
+*
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+
+typedef vector < pii > vpii;
+typedef vector < pll > vpll;
+
+typedef vector <string> vs;
+typedef vector < vi > vvi;
+typedef vector < vll > vvll;
+
+#define fl(i, a, b)	for (int i = a; i < b; ++i)
+
+#define all(v) 	(v).begin(), (v).end()
+#define srt(v)	sort (all (v))
+
+#define pb push_back
+#define mp make_pair
+
+#define dig(i)	(s[i] - '0')
+#define slen(s)	s.length()
+
+#define fr first
+#define sc second
+
+#define len(x) x.size()
+#define fill(x, y)	memset(x, y, sizeof(x))
+#define clr(a)	fill(a, 0)
+#define endl '\n'
+
+#define PI 3.14159265358979323
+
+#define trace1(x1)					cerr << #x1 << ": " << x1 << endl;
+#define trace2(x1, x2)				cerr << #x1 << ": " << x1 << " | " << #x2 << ": " << x2 << endl;
+#define trace3(x1, x2, x3)			cerr << #x1 << ": " << x1 << " | " << #x2 << ": " << x2 << " | " << #x3 << ": " << x3 << endl;
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+const ll MOD = 1000000007LL;
+const ll MAX = 100010LL;
+
+template <typename T> T gcd(T a, T b) { if (b == 0) return a; return gcd(b, a % b);}
+template <typename T> T power(T x, T y, ll m = MOD) { T ans = 1; x %= m; while (y > 0) { if (y & 1ll) ans = (ans * x) % m; y >>= 1ll; x = (x * x)%m; } return ans%m;}
+
+bool check(char c){
+  if( c != ',' and c != '.' and c != ':' and c != ';' and c != '!' and c != '?' and c != '\"' and c != '(' and c != ')' and c != '\n' and c != '\t' and c != ' '){
+    return true;
+  }
+  return false;
+}
+
+int main() {
+	FAST_IO;
+  string line;
+  int lambda = 0;
+  map<string, int> words;
+
+  while( getline(cin, line) ){
+    if( line == "****END_OF_INPUT****") break;
+    else if(  line == "****END_OF_TEXT****"  ){
+      //calculate Data
+      int n = words.size();
+      double eT = 0, eMax = 0, eRel;
+      map<string, int>::iterator it;
+      double c = log10(lambda);
+      for(it = words.begin(); it != words.end(); it++){
+          eT += ((it->second)*( c - log10(it->second)));
+      }
+      eT /= lambda;
+      eMax = c;
+      eRel = (eT/eMax)*100;
+
+      printf("%d %.1lf %0.lf\n", lambda, eT, eRel);
+
+      words = map<string, int>();
+      lambda = 0;
+    }
+    else{
+      //Process string
+      string s = "";
+      int n, i;
+      fl(i, 0, line.size()){
+        if( check(line[i]) ){
+          s += tolower(line[i]);
+        }
+        else if( s != "" ){
+          words[s] += 1;
+          s = "";
+          lambda++;
+        }
+      }
+      if( s != "" ){
+        words[s] += 1;
+        s = "";
+        lambda++;
+      }
+    }
+  }
+
+
+	return 0;
+}
