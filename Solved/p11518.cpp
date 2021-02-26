403565b22165f2971a696756275f0869906a7a17
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-02-26 03:32:58 -0500

    11518 - Dominos 2

---
+++ Solved/p11518.cpp
@@ -0,0 +1,102 @@
+/*
+*
+* Problema 11518 - Dominos 2
+*
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+
+typedef vector < pii > vpii;
+typedef vector < pll > vpll;
+
+typedef vector <string> vs;
+typedef vector < vi > vvi;
+typedef vector < vll > vvll;
+
+#define fl(i, a, b)	for (int i = a; i < b; ++i)
+
+#define all(v) 	(v).begin(), (v).end()
+#define srt(v)	sort (all (v))
+
+#define pb push_back
+#define mp make_pair
+
+#define dig(i)	(s[i] - '0')
+#define slen(s)	s.length()
+
+#define fr first
+#define sc second
+
+#define len(x) x.size()
+#define fill(x, y)	memset(x, y, sizeof(x))
+#define clr(a)	fill(a, 0)
+#define endl '\n'
+
+#define PI 3.14159265358979323
+
+#define trace1(x1)					cerr << #x1 << ": " << x1 << endl;
+#define trace2(x1, x2)				cerr << #x1 << ": " << x1 << " | " << #x2 << ": " << x2 << endl;
+#define trace3(x1, x2, x3)			cerr << #x1 << ": " << x1 << " | " << #x2 << ": " << x2 << " | " << #x3 << ": " << x3 << endl;
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+const ll MOD = 1000000007LL;
+const ll MAX = 100010LL;
+
+template <typename T> T gcd(T a, T b) { if (b == 0) return a; return gcd(b, a % b);}
+template <typename T> T power(T x, T y, ll m = MOD) { T ans = 1; x %= m; while (y > 0) { if (y & 1ll) ans = (ans * x) % m; y >>= 1ll; x = (x * x)%m; } return ans%m;}
+
+int main() {
+	FAST_IO;
+  int cases, n, m, l, i, x, y, z, t;
+
+  cin >> cases;
+
+  while(cases--){
+    cin >> n >> m >> l;
+    map<int, vector<int>> graph;
+    vector<int> visited(n, 0);
+    
+    for(i = 0; i < m; ++i){
+      cin >> x >> y;
+      graph[x].push_back(y);
+    }
+    t = 0;
+
+    for(i = 0; i < l; ++i){
+      cin >> z;
+      if(!visited[z-1]){
+        t++;
+        queue<int> cola;
+        cola.push(z);
+        visited[z-1] = 1;
+
+        while(!cola.empty()){
+          x = cola.front();
+          cola.pop();
+
+          for(int j : graph[x]){
+            if(!visited[j-1]){
+              t++;
+              cola.push(j);
+              visited[j-1] = 1;
+            }
+          }
+        }
+      }
+    }
+
+    cout << t << endl;
+  }
+
+	return 0;
+}
