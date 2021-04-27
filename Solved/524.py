5ae5b6165c567691609e3312045602d86dfd5d24
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-04-26 21:37:08 -0500

    524 - Prime Ring Problem

--- Solved/524.py
+++ Solved/524.py
@@ -8,9 +8,10 @@ from sys import stdin, stdout
 
 primes = [0 for n in range(101)]
 aux = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+memo = []
 
 def solve(lst, n):
-  global primes
+  global primes, memo
   if len(lst) == n:
     ans = ""
     for i in range(n):
@@ -20,20 +21,30 @@ def solve(lst, n):
     ans += "\n"
     stdout.write(ans)
   else:
-    for i in range(2, n+1):
-      if i not in lst and primes[lst[-1]+i]:
-        if len(lst) == n-1 and primes[lst[0]+i]:
+    for i in memo[lst[-1]]:
+      if i > n:
+        break
+      if i not in lst:
+        if len(lst) == n-1 and i in memo[lst[0]]:
           lst.append(i)
           solve(lst, n)
           lst.pop()
-        elif len(lst)  != n-1:
+        elif len(lst) != n-1:
           lst.append(i)
           solve(lst, n)
           lst.pop()
 
 def main():
+  global memo
+  memo = [ [] for _ in range(17) ]
   for i in aux:
     primes[i] = 1
+  
+  for i in range(1, 17):
+    for j in range(2, 17):
+      if primes[i+j]:
+        memo[i].append(j)
+
   lines = stdin.readlines()
   cases = 0
 
