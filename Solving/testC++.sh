e419351e9aadbb74eae98174d22e01e04c03e276
Robin Quintero <rohaquinlop301@gmail.com>
2023-08-21 21:00:33 -0500

    testC++.sh - add timeout as parameter

--- Solving/testC++.sh
+++ Solving/testC++.sh
@@ -6,8 +6,12 @@ CYAN='\e[1;36m'
 YELLOW='\e[1;33m'
 BOLD='\033[1m'
 PLAIN='\033[0m'
+FILE=./a.out
+if [ -f "$FILE" ]; then
+    rm $FILE
+fi
 g++ -std=c++11 $1
-time timeout 3 ./a.out < in.txt > out.txt
+time timeout $2 ./a.out < in.txt > out.txt
 if [ $? -eq 124 ]
   then
   echo -e "\n${RED}Error:${WHITE} Time limit exceeded.\n"
